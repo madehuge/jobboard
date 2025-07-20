e7a60ee95cbd629cd84925894e4dd2a3b9555f15
Manishjangir <manishjangir027@gmail.com>
2025-07-20 19:09:49 +0530

    React Module Started Day 1

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardItem from './CardItem';
 
 function UserCard() {
+  const [bgColors, setBgColors] = useState({
+    Manish: '#db1b1bff',
+    Demo: '#040000ff',
+  });
 
-  // This function will receive data from the child
+  const changeCardColor = (name) => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setBgColors((prevColors) => ({
+      ...prevColors,
+      [name]: randomColor,
+    }));
+  };
+
   const handleNameClick = (name) => {
     alert(`Child clicked: ${name}`);
   };
 
   return (
     <>
-      <div><CardItem name="Manish" sendToParent={handleNameClick} /></div>
-      <div><CardItem name="Demo" sendToParent={handleNameClick} /></div>
+      <CardItem
+        name="Manish"
+        sendToParent={handleNameClick}
+        bgColor={bgColors.Manish}
+        onChange={() => changeCardColor("Manish")}
+      />
+      <CardItem
+        name="Demo"
+        sendToParent={handleNameClick}
+        bgColor={bgColors.Demo}
+        onChange={() => changeCardColor("Demo")}
+      />
     </>
   );
 }
